d3bacd10620b50c3e19473c4ec0e58a4b0ef2d6e
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-09-18 11:08:10 -0400

    sdg

--- AreaPerBenF/AreaPerBenF/AreaPer.cs
+++ AreaPerBenF/AreaPerBenF/AreaPer.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace AreaPerBenF
 {
-    static class Program
+    static class AreaPer
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace AreaPerBenF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AreaPerForm());
         }
     }
 }
