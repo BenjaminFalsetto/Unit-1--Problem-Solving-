dc89708ec0928580b27b1947113613cf676ad296
sergeinichleichgolovko <32547110+SergeiNicholeichGolovko@users.noreply.github.com>
2018-10-01 18:53:54 -0400

    hmbn

---
+++ PizzaCost/PizzaCost/PizzaCostForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PizzaCost
+{
+    partial class frmPizzaCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSize = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.lblNumPizzas = new System.Windows.Forms.Label();
+            this.txtNumPizzas = new System.Windows.Forms.TextBox();
+            this.btnCalCost = new System.Windows.Forms.Button();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(38, 51);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(496, 24);
+            this.lblSize.TabIndex = 0;
+            this.lblSize.Text = "What diameter would you like your pizza(s) to have? (in)";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(549, 56);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 1;
+            // 
+            // lblNumPizzas
+            // 
+            this.lblNumPizzas.AutoSize = true;
+            this.lblNumPizzas.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumPizzas.Location = new System.Drawing.Point(38, 138);
+            this.lblNumPizzas.Name = "lblNumPizzas";
+            this.lblNumPizzas.Size = new System.Drawing.Size(300, 24);
+            this.lblNumPizzas.TabIndex = 2;
+            this.lblNumPizzas.Text = "How many pizzas would you like?";
+            // 
+            // txtNumPizzas
+            // 
+            this.txtNumPizzas.Location = new System.Drawing.Point(549, 143);
+            this.txtNumPizzas.Name = "txtNumPizzas";
+            this.txtNumPizzas.Size = new System.Drawing.Size(100, 20);
+            this.txtNumPizzas.TabIndex = 3;
+            // 
+            // btnCalCost
+            // 
+            this.btnCalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalCost.Location = new System.Drawing.Point(295, 241);
+            this.btnCalCost.Name = "btnCalCost";
+            this.btnCalCost.Size = new System.Drawing.Size(144, 28);
+            this.btnCalCost.TabIndex = 4;
+            this.btnCalCost.Text = "Calculate the cost";
+            this.btnCalCost.UseVisualStyleBackColor = true;
+            this.btnCalCost.Click += new System.EventHandler(this.btnCalCost_Click);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtotal.Location = new System.Drawing.Point(38, 310);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(307, 21);
+            this.lblSubtotal.TabIndex = 5;
+            this.lblSubtotal.Text = "This is where the subtotal will appear.";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(38, 361);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(281, 21);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "This is where the total will appear.";
+            // 
+            // frmPizzaCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.btnCalCost);
+            this.Controls.Add(this.txtNumPizzas);
+            this.Controls.Add(this.lblNumPizzas);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.lblSize);
+            this.Name = "frmPizzaCost";
+            this.Text = "Pizza Cost Benjamin Falsetto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label lblNumPizzas;
+        private System.Windows.Forms.TextBox txtNumPizzas;
+        private System.Windows.Forms.Button btnCalCost;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ PizzaCost/PizzaCost/PizzaCostForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaCost
+{
+    public partial class frmPizzaCost : Form
+    {
+        public frmPizzaCost()
+        {
+            InitializeComponent();
+            this.lblSubtotal.Hide();
+            this.lblTotal.Hide();
+        }
+
+        private void btnCalCost_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double size, numPizzas, subTotal, total;
+
+            //declare constants
+            const double RENT = 0.99, LABOURCOST = 0.75, HST = 1.13;
+
+            //assign each variable it's value
+            size = double.Parse(txtSize.Text);
+            numPizzas = double.Parse(txtNumPizzas.Text);
+
+            //calculate and display the subtotal the subtotal label
+            subTotal = (numPizzas * RENT) + (numPizzas * LABOURCOST) + (0.5 * size);
+            this.lblSubtotal.Text = Convert.ToString("Your subtotal is $" + subTotal);
+            this.lblSubtotal.Show();
+
+            //calculate and display the total the total label
+            total = HST * subTotal;
+            this.lblTotal.Text = Convert.ToString("Your total is: $" + total);
+            this.lblTotal.Show();
+        }
+    }
+}
